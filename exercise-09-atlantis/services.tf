6e1806c6735dbf055cc07269502b0f85b48ad8c7
Steven Tobias <stobias123@gmail.com>
2019-09-11 09:11:24 -0600

    fixing ssh key

--- exercise-09-atlantis/services.tf
+++ exercise-09-atlantis/services.tf
@@ -6,8 +6,7 @@ module "shopping_cart" {
   source = "../ec2_instance_module"
   name = "stobias-testing33"
   key_name = "toby-testing33"
-  public_key = ""
-  # ... add your params
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDeMRsZc/B2poz/fqEYOXuYKiM1dRP9usGUGMc6WanOs10c/YlDUKyc/Wq8gC9KEWep+1dCM2i2DvMlc3D8Q58TS09jieiX9YrCBGuLO49uW8accT3EGAIZHrO9AmpLYFnezUdkZKST+rYVlugWehZzyjBhsp3kTrsaR+qHPvv5lBzr7h7k1Coz5AyEkQayvCJ42M0aF2w7+tQbOgLBWLMrRHGWyXJohIO9xcaaQ6xMFWM9vgfMccy/H1hCCOJk4zEDO8w4VdPRr9/oqm6gddNQzJbTYV0o2AQhyK8LvHeJ6kFOBzvbJe3RI17BnsWvHs0GHZpCINyO8tT87ehgptdP steventobias@StevenTobias-MBP-MV0114.local"
 }
 
 
@@ -15,6 +14,6 @@ module "checkout" {
   source = "../ec2_instance_module"
   name = "stobias-testing3"
   key_name = "toby-testing3"
-  public_key = ""
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDeMRsZc/B2poz/fqEYOXuYKiM1dRP9usGUGMc6WanOs10c/YlDUKyc/Wq8gC9KEWep+1dCM2i2DvMlc3D8Q58TS09jieiX9YrCBGuLO49uW8accT3EGAIZHrO9AmpLYFnezUdkZKST+rYVlugWehZzyjBhsp3kTrsaR+qHPvv5lBzr7h7k1Coz5AyEkQayvCJ42M0aF2w7+tQbOgLBWLMrRHGWyXJohIO9xcaaQ6xMFWM9vgfMccy/H1hCCOJk4zEDO8w4VdPRr9/oqm6gddNQzJbTYV0o2AQhyK8LvHeJ6kFOBzvbJe3RI17BnsWvHs0GHZpCINyO8tT87ehgptdP steventobias@StevenTobias-MBP-MV0114.local"
   # ... add your params
-} */
+}
