a8c43056e106ad89ceb68a9a841f94a581ca5fc8
Steven Tobias <stobias123@gmail.com>
2019-09-11 09:10:22 -0600

    testing

--- exercise-09-atlantis/services.tf
+++ exercise-09-atlantis/services.tf
@@ -2,13 +2,19 @@
 # DEPLOY THE SERVICES
 # ---------------------------------------------------------------------------------------------------------------------
 
-/* module "shopping_cart" {
+module "shopping_cart" {
   source = "../ec2_instance_module"
+  name = "stobias-testing33"
+  key_name = "toby-testing33"
+  public_key = ""
   # ... add your params
 }
 
 
 module "checkout" {
   source = "../ec2_instance_module"
+  name = "stobias-testing3"
+  key_name = "toby-testing3"
+  public_key = ""
   # ... add your params
 } */
